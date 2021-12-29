d58aea32f638c558e22b5a30f20275cd1a673f72
Tadeusz Makuch <tadeusz.makuch@vizlib.com>
2021-12-29 16:47:09 +0100

    chore: simplify the action

--- __tests__/utils.test.ts
+++ __tests__/utils.test.ts
@@ -1,7 +1,7 @@
-import {removeIgnoreTaskLitsText, createTaskListText} from '../src/utils'
+import { removeIgnoreTaskLitsText } from "../src/utils";
 
-describe('removeIgnoreTaskLitsText', () => {
-  it('removes multiple ignore task list from task list text.', () => {
+describe("removeIgnoreTaskLitsText", () => {
+  it("removes multiple ignore task list from task list text.", () => {
     const text = `## Issue Type
     <!-- ignore-task-list-start -->
     - [ ] Bug
@@ -14,9 +14,9 @@ describe('removeIgnoreTaskLitsText', () => {
     - [x] I have made corresponding changes to the documentation
     - [x] My changes generate no lint errors
     - [x] I have added tests that prove my fix is effective or that my feature works
-    - [x] New and existing unit tests pass locally with my changes`
+    - [x] New and existing unit tests pass locally with my changes`;
 
-    const result = removeIgnoreTaskLitsText(text)
+    const result = removeIgnoreTaskLitsText(text);
 
     expect(result).toEqual(`## Issue Type
     
@@ -26,73 +26,26 @@ describe('removeIgnoreTaskLitsText', () => {
     - [x] I have made corresponding changes to the documentation
     - [x] My changes generate no lint errors
     - [x] I have added tests that prove my fix is effective or that my feature works
-    - [x] New and existing unit tests pass locally with my changes`)
-  })
+    - [x] New and existing unit tests pass locally with my changes`);
+  });
 
-  it('removes single ignore task list from task list text.', () => {
+  it("removes single ignore task list from task list text.", () => {
     const text = `<!-- ignore-task-list-start -->
     - [ ] foo
     <!-- ignore-task-list-end -->
-    - [x] bar`
+    - [x] bar`;
 
-    const result = removeIgnoreTaskLitsText(text)
+    const result = removeIgnoreTaskLitsText(text);
 
     expect(result).toEqual(`
-    - [x] bar`)
-  })
+    - [x] bar`);
+  });
 
-  it('skips remove process if task list text does not contain ignore task list.', () => {
-    const text = '- [x] bar'
+  it("skips remove process if task list text does not contain ignore task list.", () => {
+    const text = "- [x] bar";
 
-    const result = removeIgnoreTaskLitsText(text)
+    const result = removeIgnoreTaskLitsText(text);
 
-    expect(result).toEqual('- [x] bar')
-  })
-})
-
-describe('createTaskListText', () => {
-  it('creates a list of completed tasks', () => {
-    const text = `## Issue Type
-    
-    
-    ## Checklist
-    - [x] I have read the [CONTRIBUTING.md]()
-    - [x] I have made corresponding changes to the documentation
-    - [x] My changes generate no lint errors
-    - [x] I have added tests that prove my fix is effective or that my feature works
-    - [x] New and existing unit tests pass locally with my changes`
-
-    const result = createTaskListText(text)
-
-    expect(result).toEqual(`## :white_check_mark: Completed Tasks
-- [x] I have read the [CONTRIBUTING.md]()
-- [x] I have made corresponding changes to the documentation
-- [x] My changes generate no lint errors
-- [x] I have added tests that prove my fix is effective or that my feature works
-- [x] New and existing unit tests pass locally with my changes
-`)
-  })
-
-  it('creates a list of completed tasks and uncompleted tasks', () => {
-    const text = `## Issue Type
-    
-    
-    ## Checklist
-    - [x] I have read the [CONTRIBUTING.md]()
-    - [ ] I have made corresponding changes to the documentation
-    - [x] My changes generate no lint errors
-    - [ ] I have added tests that prove my fix is effective or that my feature works
-    - [x] New and existing unit tests pass locally with my changes`
-
-    const result = createTaskListText(text)
-
-    expect(result).toEqual(`## :white_check_mark: Completed Tasks
-- [x] I have read the [CONTRIBUTING.md]()
-- [x] My changes generate no lint errors
-- [x] New and existing unit tests pass locally with my changes
-## :x: Uncompleted Tasks
-- [ ] I have made corresponding changes to the documentation
-- [ ] I have added tests that prove my fix is effective or that my feature works
-`)
-  })
-})
+    expect(result).toEqual("- [x] bar");
+  });
+});
